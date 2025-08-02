33b31c57423deb5c8078aca42ba9ce98c5026638
Motasem AbuBaraka <mota6em@gmail.com>
2025-08-02 07:58:53 +0200

    Fix type errors

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import type { JWT } from "next-auth/jwt";
+import type { Session } from "next-auth";
 
 export const authOptions = {
   providers: [
@@ -9,6 +11,30 @@ export const authOptions = {
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
+
+  callbacks: {
+    async jwt({
+      token,
+      account,
+      user,
+    }: {
+      token: JWT;
+      account?: any;
+      user?: any;
+    }) {
+      if (account && user) {
+        token.id = user.id;
+      }
+
+      return token;
+    },
+    async session({ session, token }: { session: Session; token: JWT }) {
+      if (session.user) {
+        session.user.id = token.id as string;
+      }
+      return session;
+    },
+  },
 };
 
 export default NextAuth(authOptions);
