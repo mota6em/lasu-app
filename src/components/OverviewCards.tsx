68e4e4d1d5652ca3602cc0be93e92cb97bf82f6d
Motasem AbuBaraka <mota6em@gmail.com>
2025-08-06 09:52:09 +0200

    feat: enhance OverviewCards component to display dynamic translation statistics

--- src/components/OverviewCards.tsx
+++ src/components/OverviewCards.tsx
@@ -1,17 +1,54 @@
 "use client";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
+import Translation from "@/types/translation";
+import { availableLanguages } from "@/lib/languages";
 
 export function OverviewCards() {
-  const cards = [
-    { title: "Total Translations", value: "12,938" },
-    { title: "This Week", value: "189" },
-    { title: "Most Used Lang", value: "🇺🇸 English" },
-  ];
+  const [history, setHistory] = useState<Translation[]>([]);
+
   const { data: session } = useSession();
-  if (!session) {
-    return null;
-  }
+  useEffect(() => {
+    if (!session) return;
+
+    fetch("/api/translation/history")
+      .then((res) => res.json())
+      .then((data) => setHistory(data));
+  }, [session]);
+
+  if (!session) return null;
+  const totalTranslations = history.length;
+
+  const oneWeekAgo = new Date();
+  oneWeekAgo.setDate(oneWeekAgo.getDate() - 7);
+
+  const thisWeekCount = history.filter(
+    (h) => new Date(h.createdAt) > oneWeekAgo
+  ).length;
+
+  const langCount: Record<string, number> = {};
+  history.forEach((h) => {
+    Object.keys(h.result.translations).forEach((lang) => {
+      langCount[lang] = (langCount[lang] || 0) + 1;
+    });
+  });
+
+  const mostUsedLang = Object.entries(langCount).sort(
+    (a, b) => b[1] - a[1]
+  )[0]?.[0];
+
+  const cards = [
+    { title: "Total Translations", value: totalTranslations },
+    { title: "This Week", value: thisWeekCount },
+    {
+      title: "Most Used Lang",
+      value: mostUsedLang
+        ? `${availableLanguages.find((l) => l.value === mostUsedLang)?.label}  `
+        : "-",
+    },
+  ];
+
   return (
     <div className="flex flex-col gap-y-5">
       <h2 className="text-2xl font-semibold mt-2 -mb-2">Overview</h2>
