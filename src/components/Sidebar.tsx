ef0de559660c586b1df6ae10bdb32069c41ab8b0
Motasem AbuBaraka <mota6em@gmail.com>
2025-08-06 10:38:30 +0200

    Inhance UI/UX

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -30,80 +30,79 @@ export function Sidebar() {
     <aside className="w-56 bg-white border-r min-h-screen px-5 py-6 flex flex-col justify-between">
       <div>
         <TranslationSettingDialog />
-        <h2 className="text-2xl font-bold mb-8 text-gray-800 lasu-logo">
+        <h2
+          onClick={() => (window.location.href = "/")}
+          className="text-2xl font-bold mb-8 text-gray-800 lasu-logo"
+        >
           LaSu
         </h2>
-
-        {!session && (
-          <div className="group">
-            <div className="space-y-4 group">
-              <div className="flex flex-row gap-2 items-center ">
-                <img
-                  src="/imgs/sad-lasu-icon.png"
-                  alt="Sad Owl"
-                  className="w-10 h-10 group-hover:hidden"
-                />
-                <img
-                  src="/imgs/happy-lasu-icon.png"
-                  alt="Happy Owl"
-                  className="w-10 h-10 hidden group-hover:block"
-                />
-                <p className="text-sm text-gray-500 ">
-                  You're not logged in. <br />
-                  Please log in to use our full features.
-                </p>
-              </div>
-              <button
-                className="bg-blue-900 group-hover:bg-blue-700 cursor-pointer text-white text-sm px-3 py-2 rounded-md w-full group"
-                onClick={() => signIn("google")}
-              >
-                Login with Google
-              </button>
-            </div>
-          </div>
-        )}
-
-        {session && (
-          <nav className="space-y-2">
-            {menu.map((item) => {
-              const isActive = pathname === item.href;
-              if (item.label === "Settings") {
-                return (
-                  <div
-                    key={item.href}
-                    onClick={() => showSettingsDialog()}
-                    className={`${
-                      isOpen
-                        ? "bg-gray-100 text-black border border-gray-300"
-                        : ""
-                    } flex items-center gap-2 px-3 py-2 cursor-pointer rounded-md transition text-sm font-medium ${
-                      isActive
-                        ? "bg-gray-100 text-black"
-                        : "text-gray-600 hover:bg-gray-50"
-                    }`}
-                  >
-                    {item.icon}
-                    {item.label}
-                  </div>
-                );
-              }
+        <nav className="space-y-2">
+          {menu.map((item) => {
+            const isActive = pathname === item.href;
+            if (item.label === "Settings") {
               return (
-                <Link
+                <div
                   key={item.href}
-                  href={item.href}
-                  className={`flex items-center gap-2 px-3 py-2 rounded-md transition text-sm font-medium ${
-                    isActive
+                  onClick={() => showSettingsDialog()}
+                  className={`${
+                    isOpen
                       ? "bg-gray-100 text-black border border-gray-300"
+                      : ""
+                  } flex items-center gap-2 px-3 py-2 cursor-pointer rounded-md transition text-sm font-medium ${
+                    isActive
+                      ? "bg-gray-100 text-black"
                       : "text-gray-600 hover:bg-gray-50"
                   }`}
                 >
                   {item.icon}
                   {item.label}
-                </Link>
+                </div>
               );
-            })}
-          </nav>
-        )}
+            }
+            return (
+              <Link
+                key={item.href}
+                href={item.href}
+                className={`flex items-center gap-2 px-3 py-2 rounded-md transition text-sm font-medium ${
+                  isActive
+                    ? "bg-gray-100 text-black border border-gray-300"
+                    : "text-gray-600 hover:bg-gray-50"
+                }`}
+              >
+                {item.icon}
+                {item.label}
+              </Link>
+            );
+          })}
+          {!session && (
+            <div className="group absolute bottom-10 left-3 w-45">
+              <div className="space-y-4 group">
+                <div className="flex flex-row gap-2 items-center ">
+                  <img
+                    src="/imgs/sad-lasu-icon.png"
+                    alt="Sad Owl"
+                    className="w-10 h-10 group-hover:hidden"
+                  />
+                  <img
+                    src="/imgs/happy-lasu-icon.png"
+                    alt="Happy Owl"
+                    className="w-10 h-10 hidden group-hover:block"
+                  />
+                  <p className="text-sm text-gray-500 ">
+                    You're not logged in. <br />
+                    Please log in to use our full features.
+                  </p>
+                </div>
+                <button
+                  className="bg-black group-hover:bg-blue-950 cursor-pointer text-white text-sm px-3 py-2 rounded-md w-full group"
+                  onClick={() => signIn("google")}
+                >
+                  Login with Google
+                </button>
+              </div>
+            </div>
+          )}
+        </nav>
       </div>
 
       {session && (
--- src/components/UserMenu.tsx
+++ src/components/UserMenu.tsx
@@ -19,7 +19,7 @@ export function UserMenu() {
     return (
       <button
         onClick={() => signIn("google")}
-        className="text-sm px-4 py-2 rounded bg-black text-white"
+        className="text-sm px-4 py-2 rounded-md font-semibold bg-black cursor-pointer hover:bg-blue-950 text-white"
       >
         Login
       </button>
