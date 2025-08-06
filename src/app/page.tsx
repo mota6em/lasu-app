c5a3d4a1b980d7fa0bae89b9f8e6a6407b938f28
Motasem AbuBaraka <mota6em@gmail.com>
2025-08-06 11:40:50 +0200

    feat: update sitemap and add metadata for improved SEO and navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,10 @@
+import { Metadata } from "next";
 import { redirect } from "next/navigation";
+export const metadata: Metadata = {
+  title: "LaSu - Your AI Language Support",
+  description:
+    "Learn languages effortlessly with LaSu - the Chrome extension that offers AI-powered translations, real-life examples, and contextual understanding as you browse.",
+};
 
 export default function Home() {
   redirect("/dashboard");
