bf30585a03a40f77efd9a58c3d644ea7358bf43c
Motasem AbuBaraka <mota6em@gmail.com>
2025-08-13 18:43:14 +0200

    refactor: add x padding in dashboard page for responsive design

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,7 +3,7 @@ import { Translate } from "@/components/Translate";
 
 export default function DashboardPage() {
   return (
-    <div className="flex flex-col bg-white dark:bg-zinc-900">
+    <div className="flex flex-col bg-white dark:bg-zinc-900 px-3 lg:px-0">
       <h2 className="text-4xl font-bold mb-6">Dashboard</h2>
       <div className="flex flex-col gap-y-4">
         <Translate />
