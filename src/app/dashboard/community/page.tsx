acf42db1d33ddefee1818bd4e9d926213aa8e860
Motasem AbuBaraka <mota6em@gmail.com>
2025-09-05 12:02:18 +0200

    feat: Enhance community page to check user session and membership status

--- src/app/dashboard/community/page.tsx
+++ src/app/dashboard/community/page.tsx
@@ -1,5 +1,18 @@
 import JoinCommHero from "@/components/JoinCommHero";
+import { connectToDB } from "@/lib/mongodb";
+import CommunityUser from "@/models/communityUser";
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
+import { getServerSession } from "next-auth";
 
-export default function page() {
-  return <JoinCommHero />;
+export default async function page() {
+  const session = await getServerSession(authOptions);
+  if (!session?.user?.id) return <JoinCommHero />;
+
+  await connectToDB();
+  const member = await CommunityUser.findOne({ userId: session.user.id });
+
+  if (!member) {
+    return <JoinCommHero />;
+  }
+  return <div>Community</div>;
 }
