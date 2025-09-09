85c5f181dabb25cefcf5259f829dee360a43505f
Motasem AbuBaraka <mota6em@gmail.com>
2025-09-09 09:52:21 +0200

    feat: Integrate react-countup for animated statistics in CommHero component

--- src/components/CommHero.tsx
+++ src/components/CommHero.tsx
@@ -1,34 +1,38 @@
 "use client";
-import { motion } from "framer-motion";
+import CountUp from "react-countup";
+import { MdOutlineCelebration } from "react-icons/md";
 
 export default function CommHero({ userName }: { userName: string }) {
   return (
-    <motion.div
-      initial={{ opacity: 0, y: 20 }}
-      animate={{ opacity: 1, y: 0 }}
-      transition={{ duration: 0.6 }}
-      className="p-6 text-center flex flex-col justify-center items-center"
-    >
-      <h1 className="text-4xl font-bold text-yellow-600">
-        Welcome back, {userName.split(" ")[0]}! 🎉
+    <div className="p-6 text-center flex flex-col justify-center items-center">
+      <h1 className="text-4xl font-semibold flex items-center gap-2 animate-bounce">
+        Hi{" "}
+        <span className="italic text-yellow-500">{userName.split(" ")[0]}</span>{" "}
+        <MdOutlineCelebration className="" />
       </h1>
-      <p className="text-lg text-gray-500">
-        Here is what your mates are learning. Join the fun!
+      <p className="text-lg text-gray-500 dark:text-gray-400">
+        Here’s what your mates are learning — join the fun!
       </p>
       <div className="flex w-md justify-center gap-6 mt-6 text-center">
         <div className="bg-purple-700/5 p-3 rounded-xl flex-1 shadow-md">
           <h3 className="text-yellow-400 text-md font-bold">XP</h3>
-          <p className="text-white text-xl font-extrabold">1,250</p>
+          <p className="text-white text-xl font-extrabold">
+            <CountUp start={0} end={1250} duration={2} />
+          </p>
         </div>
         <div className="bg-purple-700/5 p-3 rounded-xl flex-1 shadow-md">
           <h3 className="text-yellow-400 text-md font-bold">Level</h3>
-          <p className="text-white text-xl font-extrabold">12</p>
+          <p className="text-white text-xl font-extrabold">
+            <CountUp start={1} end={12} duration={2} />
+          </p>
         </div>
         <div className="bg-purple-700/5 p-3 rounded-xl flex-1 shadow-md">
           <h3 className="text-yellow-400 text-md font-bold">Streak</h3>
-          <p className="text-white text-xl font-extrabold">7 days</p>
+          <p className="text-white text-xl font-extrabold">
+            <CountUp start={1} end={7} duration={2} /> days
+          </p>
         </div>
       </div>
-    </motion.div>
+    </div>
   );
 }
