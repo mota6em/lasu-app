54fa0c0489ad9f2746e98ef0b1eac1339282b3b1
Motasem AbuBaraka <mota6em@gmail.com>
2025-09-12 12:20:35 +0200

    Feature: allow y scroll for showing live translations cards for better ux

--- src/components/CommunityLiveTranslations.tsx
+++ src/components/CommunityLiveTranslations.tsx
@@ -76,7 +76,7 @@ export default function CommunityTranslations() {
         <Badge variant="secondary">{translations.length}</Badge>
       </h2>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 h-[400px] overflow-y-scroll">
         {translations.map((t, idx) => {
           const langs = Object.keys(
             t.result.translations
