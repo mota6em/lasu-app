5437284c1e3941e57cdb6f0c58b7b6e2679aba53
Motasem AbuBaraka <mota6em@gmail.com>
2025-09-14 23:18:17 +0200

    Feat: show loading spinner while fetching community translations

--- src/components/CommunityLiveTranslations.tsx
+++ src/components/CommunityLiveTranslations.tsx
@@ -35,14 +35,15 @@ export default function CommunityTranslations() {
   };
 
   const [translations, setTranslations] = useState<CommunityTranslation[]>([]);
-
+  const [isLoading, setIsLoading] = useState(false);
   useEffect(() => {
     const fetchTranslations = async () => {
       try {
+        setIsLoading(true);
         const res = await fetch("/api/community/live");
         const data = await res.json();
         setTranslations(data);
-        console.log("translations data", data);
+        setIsLoading(false);
       } catch (err) {
         console.error(err);
       }
@@ -75,90 +76,96 @@ export default function CommunityTranslations() {
         <FaChartLine /> Live Community Translations
         <Badge variant="secondary">{translations.length}</Badge>
       </h2>
-
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 h-[400px] overflow-y-scroll">
-        {translations.map((t, idx) => {
-          const langs = Object.keys(
-            t.result.translations
-          ) as (keyof typeof t.result.translations)[];
-          const selectedLang =
-            (selectedLangs[idx] as keyof typeof t.result.translations) ||
-            langs[0];
-          const translation = t.result.translations[selectedLang];
-          const example = t.result.example[selectedLang];
-          return (
-            <div
-              key={idx}
-              className="relative  p-4 border rounded-xl shadow hover:shadow-lg bg-blue-950/5 dark:bg-blue-950/10 transition"
-            >
-              <div className="mb-3 flex items-center justify-between">
-                <div>
-                  <h3 className="text-lg font-bold text-foreground">
-                    {t.sourceText[0].toUpperCase() + t.sourceText.slice(1)}
-                  </h3>
-                  <p className="text-xs text-muted-foreground">
-                    {t.translationType} • {t.translationFilter}
-                  </p>
-                </div>
-                <div className="flex flex-row gap-x-1 items-center justify-center text-xs text-muted-foreground">
-                  <Image
-                    src={t.userImage ?? "/imgs/userIcon.jpg"}
-                    alt="User Avatar"
-                    width={20}
-                    height={20}
-                    className="rounded-full"
-                  />
-                  <span className=" font-semibold ">
-                    {t.userName! || "Anonymous"}
-                  </span>
-                </div>
-              </div>
-
-              <div className="flex flex-wrap gap-2 mb-3">
-                {langs.map((lang) => (
-                  <Button
-                    key={lang}
-                    className="text-xs px-2 py-0.5"
-                    variant={selectedLang === lang ? "default" : "outline"}
-                    onClick={() => selectLanguage(idx.toString(), lang)}
-                  >
-                    {lang}
-                  </Button>
-                ))}
-              </div>
-
-              <div className="space-y-2">
-                <div className="flex justify-between items-center">
-                  <span className="font-semibold text-blue-600 dark:text-blue-400">
-                    {selectedLang}
-                  </span>
-                  <Button
-                    size="sm"
-                    variant="ghost"
-                    onClick={() =>
-                      speakText(translation, selectedLang, idx.toString())
-                    }
-                    disabled={audioLoading[idx]}
-                  >
-                    {audioLoading[idx] ? (
-                      <FaSpinner className="animate-spin" />
-                    ) : (
-                      <FaVolumeUp />
-                    )}
-                  </Button>
-                </div>
-
-                <div className="text-lg font-bold">{translation}</div>
-                {example && (
-                  <div className="text-sm text-muted-foreground border-l-4 border-green-400 pl-2">
-                    "{example}"
+      {(isLoading || translations.length === 0) && (
+        <div className="flex justify-center items-center">
+          <FaSpinner className="animate-spin" />
+        </div>
+      )}
+      {!isLoading && translations.length > 0 && (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 h-[400px] overflow-y-scroll">
+          {translations.map((t, idx) => {
+            const langs = Object.keys(
+              t.result.translations
+            ) as (keyof typeof t.result.translations)[];
+            const selectedLang =
+              (selectedLangs[idx] as keyof typeof t.result.translations) ||
+              langs[0];
+            const translation = t.result.translations[selectedLang];
+            const example = t.result.example[selectedLang];
+            return (
+              <div
+                key={idx}
+                className="relative  p-4 border rounded-xl shadow hover:shadow-lg bg-blue-950/5 dark:bg-blue-950/10 transition"
+              >
+                <div className="mb-3 flex items-center justify-between">
+                  <div>
+                    <h3 className="text-lg font-bold text-foreground">
+                      {t.sourceText[0].toUpperCase() + t.sourceText.slice(1)}
+                    </h3>
+                    <p className="text-xs text-muted-foreground">
+                      {t.translationType} • {t.translationFilter}
+                    </p>
                   </div>
-                )}
+                  <div className="flex flex-row gap-x-1 items-center justify-center text-xs text-muted-foreground">
+                    <Image
+                      src={t.userImage ?? "/imgs/userIcon.jpg"}
+                      alt="User Avatar"
+                      width={20}
+                      height={20}
+                      className="rounded-full"
+                    />
+                    <span className=" font-semibold ">
+                      {t.userName! || "Anonymous"}
+                    </span>
+                  </div>
+                </div>
+
+                <div className="flex flex-wrap gap-2 mb-3">
+                  {langs.map((lang) => (
+                    <Button
+                      key={lang}
+                      className="text-xs px-2 py-0.5"
+                      variant={selectedLang === lang ? "default" : "outline"}
+                      onClick={() => selectLanguage(idx.toString(), lang)}
+                    >
+                      {lang}
+                    </Button>
+                  ))}
+                </div>
+
+                <div className="space-y-2">
+                  <div className="flex justify-between items-center">
+                    <span className="font-semibold text-blue-600 dark:text-blue-400">
+                      {selectedLang}
+                    </span>
+                    <Button
+                      size="sm"
+                      variant="ghost"
+                      onClick={() =>
+                        speakText(translation, selectedLang, idx.toString())
+                      }
+                      disabled={audioLoading[idx]}
+                    >
+                      {audioLoading[idx] ? (
+                        <FaSpinner className="animate-spin" />
+                      ) : (
+                        <FaVolumeUp />
+                      )}
+                    </Button>
+                  </div>
+
+                  <div className="text-lg font-bold">{translation}</div>
+                  {example && (
+                    <div className="text-sm text-muted-foreground border-l-4 border-green-400 pl-2">
+                      "{example}"
+                    </div>
+                  )}
+                </div>
               </div>
-            </div>
-          );
-        })}
-      </div>
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 }
