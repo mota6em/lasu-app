78eebb97a105563af09fa171a3af663d8645f0e0
Motasem AbuBaraka <mota6em@gmail.com>
2025-09-18 20:07:34 +0200

    Feat: Show skeleton cards while fetching translations for first time

--- src/components/CommunityLiveTranslations.tsx
+++ src/components/CommunityLiveTranslations.tsx
@@ -84,8 +84,36 @@ export default function CommunityTranslations() {
         <Badge variant="secondary">{translations.length}</Badge>
       </h2>
       {(isLoading || translations.length === 0) && (
-        <div className="flex justify-center items-center">
-          <FaSpinner className="animate-spin" />
+        <div className="  justify-center items-center grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+          {Array.from({ length: 6 }).map((_, i) => (
+            <div className="relative p-4 border rounded-xl shadow animate-pulse bg-gray-200 dark:bg-neutral-800/20">
+              <div className="mb-3 flex items-center justify-between">
+                <div>
+                  <div className="h-4 bg-gray-300 dark:bg-neutral-700 rounded w-16 mb-1"></div>
+                  <div className="h-3 bg-gray-300 dark:bg-neutral-700 rounded w-10"></div>
+                </div>
+                <div className="flex flex-row gap-x-1 items-center">
+                  <div className="w-5 h-5 rounded-full bg-gray-300 dark:bg-neutral-700"></div>
+                  <div className="h-3 bg-gray-300 dark:bg-neutral-700 rounded w-16"></div>
+                </div>
+              </div>
+
+              <div className="flex flex-wrap gap-2 mb-3">
+                {Array.from({ length: 3 }).map((_, i) => (
+                  <div
+                    key={i}
+                    className="h-6 w-12 rounded bg-gray-300 dark:bg-neutral-700"
+                  ></div>
+                ))}
+              </div>
+
+              <div className="space-y-2">
+                <div className="h-3 bg-gray-300 dark:bg-neutral-700 rounded w-16"></div>
+                <div className="h-6 bg-gray-300 dark:bg-neutral-700 rounded w-full mt-2"></div>
+                <div className="h-4 bg-gray-300 dark:bg-neutral-700 rounded w-3/4"></div>
+              </div>
+            </div>
+          ))}
         </div>
       )}
       {!isLoading && translations.length > 0 && (
