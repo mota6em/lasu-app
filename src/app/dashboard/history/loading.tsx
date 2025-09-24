305524bf73563500be445d1ea9f67cc95c937f51
Motasem AbuBaraka <mota6em@gmail.com>
2025-09-24 19:38:21 +0200

    feat: add loading state for main pages

---
+++ src/app/dashboard/history/loading.tsx
@@ -0,0 +1,8 @@
+export default function Loading() {
+  return (
+    <div className="flex items-center gap-x-2 justify-center min-h-[300px] animate-pulse">
+      <h1 className="text-2xl font-bold">Wellcome to the LaSu History!</h1>
+      <span className="loading loading-dots loading-xl"></span>
+    </div>
+  );
+}
---
+++ src/app/dashboard/stats/loading.tsx
@@ -0,0 +1,8 @@
+export default function Loading() {
+  return (
+    <div className="flex items-center gap-x-2 justify-center min-h-[300px] animate-pulse">
+      <h1 className="text-2xl font-bold">Loading Your LaSu Stats!</h1>
+      <span className="loading loading-dots loading-xl"></span>
+    </div>
+  );
+}
