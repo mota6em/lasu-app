ea6eed19bfada862c09db4f954088f8f027b96e6
Motasem AbuBaraka <mota6em@gmail.com>
2025-10-08 23:55:08 +0200

    Feat(community): Enhance CommTopStats component with separate toggle buttons for Top Learners, Top Words, and Top Languages tables

--- src/components/pages/community/CommTopStats.tsx
+++ src/components/pages/community/CommTopStats.tsx
@@ -20,7 +20,9 @@ const CommTopStats = ({ userId }: { userId: string }) => {
     useTopLearners("monthly");
   const { learners: topAllTime, loading: loadingAll } =
     useTopLearners("allTime");
-  const [showTable, setShowTable] = useState(false);
+  const [showTopLearnersTable, setShowTopLearnersTable] = useState(false);
+  const [showTopWordsTable, setShowTopWordsTable] = useState(false);
+  const [showTopLangsTable, setShowTopLangsTable] = useState(false);
 
   interface StatItem {
     _id: string;
@@ -64,87 +66,138 @@ const CommTopStats = ({ userId }: { userId: string }) => {
       {/*from TopLearners Component */}
       <div className="flex flex-col items-center gap-2">
         <UserRanks userId={userId} />
-        <button
-          onClick={() => setShowTable(!showTable)}
-          className="bg-gradient-to-r text-sm dark:from-purple-600/20 from-purple-950/90 dark:to-yellow-500/20 to-yellow-700/90 hover:from-purple-700/60 hover:to-yellow-600/60 text-white font-semibold py-1 px-3 rounded-lg cursor-pointer shadow-lg flex items-center gap-2 transition-all duration-300 hover:scale-105 hover:rounded-xl"
-        >
-          {showTable ? (
-            <>
-              <FaTimes className="dark:text-yellow-300" /> Hide Leaderboard
-            </>
-          ) : (
-            <>
-              <FaStar className="text-yellow-300 animate-pulse" size={20} />{" "}
-              Show Leaderboard
-            </>
-          )}
-        </button>
+        <div>
+          <button
+            onClick={() => {
+              setShowTopLearnersTable(!showTopLearnersTable);
+              setShowTopWordsTable(false);
+              setShowTopLangsTable(false);
+            }}
+            className="bg-gradient-to-r text-sm dark:from-purple-600/20 from-purple-950/90 dark:to-yellow-500/20 to-yellow-700/90 hover:from-purple-700/60 hover:to-yellow-600/60 text-white font-semibold py-1 px-3 rounded-lg cursor-pointer shadow-lg flex items-center gap-2 transition-all duration-300 hover:scale-105 hover:rounded-xl"
+          >
+            {showTopLearnersTable ? (
+              <>
+                <FaTimes className="dark:text-yellow-300" /> Hide Leaderboard
+              </>
+            ) : (
+              <>
+                <FaStar className="text-yellow-300 animate-pulse" size={20} />{" "}
+                Show Leaderboard
+              </>
+            )}
+          </button>
+          <button
+            onClick={() => {
+              setShowTopLearnersTable(false);
+              setShowTopWordsTable(!showTopWordsTable);
+              setShowTopLangsTable(false);
+            }}
+            className="bg-gradient-to-r text-sm dark:from-purple-600/20 from-purple-950/90 dark:to-yellow-500/20 to-yellow-700/90 hover:from-purple-700/60 hover:to-yellow-600/60 text-white font-semibold py-1 px-3 rounded-lg cursor-pointer shadow-lg flex items-center gap-2 transition-all duration-300 hover:scale-105 hover:rounded-xl"
+          >
+            {showTopWordsTable ? (
+              <>
+                <FaTimes className="dark:text-yellow-300" /> Hide Top Translated
+                Words
+              </>
+            ) : (
+              <>
+                <FaStar className="text-yellow-300 animate-pulse" size={20} />{" "}
+                Show Top Translated Words
+              </>
+            )}
+          </button>
+          <button
+            onClick={() => {
+              setShowTopLearnersTable(false);
+              setShowTopWordsTable(false);
+              setShowTopLangsTable(!showTopLangsTable);
+            }}
+            className="bg-gradient-to-r text-sm dark:from-purple-600/20 from-purple-950/90 dark:to-yellow-500/20 to-yellow-700/90 hover:from-purple-700/60 hover:to-yellow-600/60 text-white font-semibold py-1 px-3 rounded-lg cursor-pointer shadow-lg flex items-center gap-2 transition-all duration-300 hover:scale-105 hover:rounded-xl"
+          >
+            {showTopLangsTable ? (
+              <>
+                <FaTimes className="dark:text-yellow-300" /> Hide Top Translated
+                Languages
+              </>
+            ) : (
+              <>
+                <FaStar className="text-yellow-300 animate-pulse" size={20} />{" "}
+                Show Top Translated Languages
+              </>
+            )}
+          </button>
+        </div>
       </div>
       {/*from LangsWordsChart component */}
+
       <div className="flex flex-col items-center justify-center my-10 gap-y-8">
-        <div className="w-full">
-          <h2 className="text-2xl font-bold text-center mb-5 flex items-center justify-center gap-x-2 ">
-            <FiTrendingUp className="w-6 h-6 text-yellow-500 mt-1" />
-            Top used Words
-            <FiTrendingUp className="w-6 h-6 text-yellow-500 mt-1 -ms-1" />
-          </h2>
-          <div className="flex flex-col md:flex-row flex-wrap gap-4 items-center md:items-start justify-center">
-            <ExpandableList
-              title="Today"
-              items={stats.daily.words.map((w: any) => ({
-                word: w._id,
-                count: w.count,
-              }))}
-            />
-            <ExpandableList
-              title="This Month"
-              items={stats.monthly.words.map((w: any) => ({
-                word: w._id,
-                count: w.count,
-              }))}
-            />
-            <ExpandableList
-              title="All Time"
-              items={stats.allTime.words.map((w: any) => ({
-                word: w._id,
-                count: w.count,
-              }))}
-            />
+        {showTopWordsTable && (
+          <div className="w-full">
+            <h2 className="text-2xl font-bold text-center mb-5 flex items-center justify-center gap-x-2 ">
+              <FiTrendingUp className="w-6 h-6 text-yellow-500 mt-1" />
+              Top used Words
+              <FiTrendingUp className="w-6 h-6 text-yellow-500 mt-1 -ms-1" />
+            </h2>
+            <div className="flex flex-col md:flex-row flex-wrap gap-4 items-center md:items-start justify-center">
+              <ExpandableList
+                title="Today"
+                items={stats.daily.words.map((w: any) => ({
+                  word: w._id,
+                  count: w.count,
+                }))}
+              />
+              <ExpandableList
+                title="This Month"
+                items={stats.monthly.words.map((w: any) => ({
+                  word: w._id,
+                  count: w.count,
+                }))}
+              />
+              <ExpandableList
+                title="All Time"
+                items={stats.allTime.words.map((w: any) => ({
+                  word: w._id,
+                  count: w.count,
+                }))}
+              />
+            </div>
           </div>
-        </div>
-        <div className="w-full">
-          <h2 className="text-2xl font-bold text-center mb-5 flex items-center justify-center gap-x-2 ">
-            <FiTrendingUp className="w-6 h-6 text-yellow-500 mt-1" />
-            Top used Languages
-            <FiTrendingUp className="w-6 h-6 text-yellow-500 mt-1 -ms-1" />
-          </h2>
-          <div className="flex flex-col w-full md:flex-row flex-wrap gap-4 items-center md:items-start justify-center">
-            <ExpandableList
-              title="Today"
-              items={stats.daily.languages.map((l: any) => ({
-                word: l._id,
-                count: l.count,
-              }))}
-            />
-            <ExpandableList
-              title="This Month"
-              items={stats.monthly.languages.map((l: any) => ({
-                word: l._id,
-                count: l.count,
-              }))}
-            />
-            <ExpandableList
-              title="All Time"
-              items={stats.allTime.languages.map((l: any) => ({
-                word: l._id,
-                count: l.count,
-              }))}
-            />
+        )}
+        {showTopLangsTable && (
+          <div className="w-full">
+            <h2 className="text-2xl font-bold text-center mb-5 flex items-center justify-center gap-x-2 ">
+              <FiTrendingUp className="w-6 h-6 text-yellow-500 mt-1" />
+              Top used Languages
+              <FiTrendingUp className="w-6 h-6 text-yellow-500 mt-1 -ms-1" />
+            </h2>
+            <div className="flex flex-col w-full md:flex-row flex-wrap gap-4 items-center md:items-start justify-center">
+              <ExpandableList
+                title="Today"
+                items={stats.daily.languages.map((l: any) => ({
+                  word: l._id,
+                  count: l.count,
+                }))}
+              />
+              <ExpandableList
+                title="This Month"
+                items={stats.monthly.languages.map((l: any) => ({
+                  word: l._id,
+                  count: l.count,
+                }))}
+              />
+              <ExpandableList
+                title="All Time"
+                items={stats.allTime.languages.map((l: any) => ({
+                  word: l._id,
+                  count: l.count,
+                }))}
+              />
+            </div>
           </div>
-        </div>
+        )}
       </div>
       {/*from TopLearners Component */}
-      {showTable && (
+      {showTopLearnersTable && (
         <div className="mt-6 mx-2 md:mx-0 p-6 bg-gray-100 dark:bg-purple-900/5 rounded-2xl shadow-lg text-black dark:text-white">
           <div className="flex flex-col lg:flex-row gap-6">
             {RenderSection("Top Today", <FaClock />, topDay, loadingDay)}
