140c86e4f8d509f160d48ea645b2578e675e77ab
Motasem AbuBaraka <mota6em@gmail.com>
2025-10-11 21:56:57 +0200

    Style(practice): adjust layout and text alignment in PracticePage for improved visual consistency

--- src/app/dashboard/practice/page.tsx
+++ src/app/dashboard/practice/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-import React, { useEffect } from "react";
 import { Brain } from "lucide-react";
 import PracticeCard from "@/components/pages/practice/PracticeCard";
 import { ProgressStats } from "@/components/pages/practice/ProgressStats";
@@ -7,14 +6,14 @@ import { ProgressStats } from "@/components/pages/practice/ProgressStats";
 const PracticePage = () => {
   return (
     <div className="flex flex-col -mt-26 md:-mt-4 items-center justify-center min-h-screen md:p-6 ">
-      <div className="mb-8 text-center">
+      <div className="mb-8 flex flex-col items-center text-center">
         <h1 className="text-4xl font-bold flex items-center gap-2  text-indigo-800 dark:text-indigo-400">
           <Brain className="w-8 h-8 text-indigo-800 dark:text-indigo-300" />
           Practice Hub
           <Brain className="w-8 h-8 text-indigo-800 dark:text-indigo-300" />
         </h1>
         <p className="text-gray-600 dark:text-gray-400">
-          Sharpen your memory, one word at a time ✨
+          ✨ Sharpen your memory, one word at a time ✨
         </p>
       </div>
       <PracticeCard />
