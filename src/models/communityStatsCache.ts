d633b4b37a7a2a95d5daf8366eaf75488859a3ad
Motasem AbuBaraka <mota6em@gmail.com>
2025-10-28 19:22:35 +0100

    refactor(communityStatsCashe): update learners schema to use LearnerSchema for consistency

--- src/models/communityStatsCache.ts
+++ src/models/communityStatsCache.ts
@@ -1,9 +1,27 @@
 import mongoose, { Schema, models } from "mongoose";
 
-const StatItemSchema = new Schema({
-  _id: { type: String, required: true },
-  count: { type: Number, required: true },
-});
+const StatItemSchema = new Schema(
+  {
+    _id: { type: String, required: true },
+    count: { type: Number, required: true },
+  },
+  {
+    _id: false,
+  }
+);
+
+const LearnerSchema = new Schema(
+  {
+    id: { type: String, required: true },
+    name: { type: String, required: true },
+    image: { type: String },
+    xp: { type: Number, default: 0 },
+    showName: { type: Boolean, default: true },
+    showPicture: { type: Boolean, default: true },
+    totalTranslations: { type: Number, default: 0 },
+  },
+  { _id: false }
+);
 
 const CommunityStatsCacheSchema = new Schema({
   lastUpdated: { type: Date, required: true },
@@ -11,19 +29,19 @@ const CommunityStatsCacheSchema = new Schema({
   daily: {
     words: [StatItemSchema],
     languages: [StatItemSchema],
-    learners: [StatItemSchema],
+    learners: [LearnerSchema],
   },
 
   monthly: {
     words: [StatItemSchema],
     languages: [StatItemSchema],
-    learners: [StatItemSchema],
+    learners: [LearnerSchema],
   },
 
   allTime: {
     words: [StatItemSchema],
     languages: [StatItemSchema],
-    learners: [StatItemSchema],
+    learners: [LearnerSchema],
   },
 });
 
