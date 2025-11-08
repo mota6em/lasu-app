469c38e4cb6437235bd1ca943593a58e74825777
Motasem AbuBaraka <mota6em@gmail.com>
2025-11-09 00:27:58 +0100

    refactor(PracticePage): remove ProgressStats component and adjust layout

--- src/app/dashboard/practice/page.tsx
+++ src/app/dashboard/practice/page.tsx
@@ -1,11 +1,10 @@
 "use client";
 import { Brain } from "lucide-react";
 import PracticeCard from "@/components/pages/practice/PracticeCard";
-import { ProgressStats } from "@/components/pages/practice/ProgressStats";
-
+ 
 const PracticePage = () => {
   return (
-    <div className="flex flex-col -mt-26 md:-mt-4 items-center justify-center min-h-screen md:p-6 ">
+    <div className="flex flex-col pt-6 md:pt-4 items-center justify-center min-h-screen md:p-6 ">
       <div className="mb-8 flex flex-col items-center text-center">
         <h1 className="text-4xl font-bold flex items-center gap-2  text-indigo-800 dark:text-indigo-400">
           <Brain className="w-8 h-8 text-indigo-800 dark:text-indigo-300" />
@@ -17,9 +16,9 @@ const PracticePage = () => {
         </p>
       </div>
       <PracticeCard />
-      <ProgressStats />
-    </div>
+     </div>
   );
 };
 
 export default PracticePage;
+
